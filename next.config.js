62c289e72da9df129beae5c6c5316eaaef6a40a5
VanPete <williamvanpietersom@yahoo.com>
2025-08-24 17:12:31 -0700

    chore: restore next.config to default and remove .next_build from repo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // Use a local build directory to avoid OneDrive/Windows readlink issues
-  distDir: '.next_build',
-};
+/** @type {import('next').NextConfig} */
+module.exports = {};
